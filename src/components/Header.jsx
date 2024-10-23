15a166dfbcdc33977fbe930d1e8ab6a614e5d329
Cleber Franca <cleber.francaa12@gmail.com>
2024-10-23 19:11:16 -0300

    header estilizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import styles from '../components/Header.module.css';
 
+
 function Header() {
     return (
         <>
+        <div className={styles.headerGeral}>
             <article className={styles.menuPrincipal}>
-                <div id="logo">
+                <div className={styles.logo}>
                     <img src="../public/logo.png" alt="logo digital store" />
                 </div>
 
@@ -15,30 +17,31 @@ function Header() {
                     </a>
                 </div>
 
-                <div id="login">
+                <div className={styles.login}>
                     <a href="#">Cadastre-se</a>
-                    <button className="btn botaoRosa"  type="button">Entrar</button>
+                    <button className={`btn ${styles.botaoRosa}`} type="button">Entrar</button>
                 </div>
 
                 <div id="carrinho">
                     <button style={{ background: 'none' }} type="button" className="btn position-relative">
                         <img src="../public/Buy.png" alt="carrinho de compra" />
                         <span className="position-absolute top-0 start-75 translate-middle badge rounded-pill" style={{ backgroundColor: '#C92071' }}>
-                            1
+                            2
                             <span className="visually-hidden">Quantidade comprada</span>
                         </span>
                     </button>
                 </div>
             </article>
 
-            <nav id="menuNavegacao">
+            <nav className={styles.menuNavegacao}>
                 <ul className={styles.listas}>
-                    <li><a id="menuAtivo" href="#">Home</a></li>
+                    <li><a className={styles.menuAtivo} href="#">Home</a></li>
                     <li><a href="#">Produtos</a></li>
                     <li><a href="#">Categorias</a></li>
                     <li><a href="#">Meus Pedidos</a></li>
                 </ul>
             </nav>
+            </div>
         </>
     );
 }
